2ba01825ba47a807fa9041a9702511452b32378f
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2025-05-01 13:23:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     profile_url = "https://lubimyczytac.pl/profil/605200/stokuj/biblioteczka/lista?page=1&listId=booksFilteredList&findString=&kolejnosc=data-dodania&listType=list&objectId=605200&own=0&paginatorType=Standard"
     books = scrape_books(profile_url)
     save_books_to_csv(books, 'dane/books.csv')
+    print(f"Scraped {len(books)} books and saved to 'dane/books.csv'")
